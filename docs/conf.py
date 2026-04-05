94cb271aeb12f2ace0346bf67391a997b6954d26
minwook-shin <minwook0106@gmail.com>
2026-04-05 14:57:08 +0900

    add Read the Docs setting

---
+++ docs/conf.py
@@ -0,0 +1,10 @@
+project = 'notion-database'
+copyright = '2021, minwook-shin'
+author = 'minwook-shin'
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+html_theme = 'sphinx_rtd_theme'
